6a310474a59c3bf5c59803513187b917aabc1068
notjoms <jgp5232002@gmail.com>
2017-05-09 21:50:35 -0500

    Update weatherCollect.py

--- WeatherPiPythonDataCollection/weatherCollect.py
+++ WeatherPiPythonDataCollection/weatherCollect.py
@@ -1,18 +1,18 @@
 import pyowm as OWM
 import time
 import os
-owm = OWM.OWM('388dfaa254e9e7cade977c9fc818581e')
+owm = OWM.OWM('your_api_key_here')
 cwd = os.getcwd()
 os.chdir('..')
 os.chdir('WeatherPiJavaGui')
 msRunning=0
-weather2 = owm.weather_at_place("College station, us")
+weather2 = owm.weather_at_place("your_location_here")
 weather3 = weather2.get_weather()
 weather4 = weather3.get_temperature('celsius')
 weather5 = weather4['temp']
 while True:
     if msRunning % 360000 == 0:
-        weather2 = owm.weather_at_id(4682464)
+        weather2 = owm.weather_at_place("your_location_here")
         weather3 = weather2.get_weather()
         weather4 = weather3.get_temperature('celsius')
         weather5 = weather4['temp']
